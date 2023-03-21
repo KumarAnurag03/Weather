8ed0d453a1b71bae675da1781767664bbf4b21e2
Anurag Kumar Singh <anuragkrsingh2503@gmail.com>
2023-03-21 20:11:40 +0530

    Weather made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [city, setCity] = useState(null);
+  const [weather, setweather] = useState(null);
+
+  useEffect(() => {
+    async function temp(){
+      const instance = axios.create();
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7f204b0d1bcb330534811e0d775cd3f7`
+       try {
+        const response = await instance.get(url);
+        console.log(response);
+        setweather(response);
+       } catch (error) {
+        setweather(null);
+        console.log(error);
+       }
+       
+
+    }
+    temp();
+  
+    
+  }, [city])
+
+  let MyBackgroundImage='./picture/Screenshot_20230321_130216_WhatsApp.jpg';
+  if(weather) MyBackgroundImage=`https://source.unsplash.com/1600x900/?${city}`;
+
+  return(
+    <div className='app' 
+    style={{backgroundImage:`url(${MyBackgroundImage})`}}>                                                                                                                                                                                                    >
+      <div className='search-bar'>
+        <input type="search" 
+          placeholder='Enter Your City'
+           onChange={(e)=>setCity(e.target.value)}
+        />
+      </div>
+      {
+        !weather? (
+          <h2 id='cent'>No Data found</h2>
+        ):
+        (
+          <div className="app-contain">
+            <div className="top">
+              <div className="location">
+                <h2>{city}</h2>
+              </div>
+              <div className="temp">
+                <h1>{weather.data.main.feels_like}&deg; C</h1>
+              </div>
+              <div className="desc">
+                <h3>{weather.data.weather[0].main}</h3>
+              </div>
+            </div>
+            <div className="bottom">
+              <div className="maximum">
+                <p>{weather.data.main.temp_max}&deg;C</p>
+                <p>Maximum Temperature</p>
+              </div>
+              <div className="minimum">
+                <p>{weather.data.main.temp_min}&deg;C</p>
+                <p>Minimum Temperature</p>
+              </div>
+              <div className="humidity">
+                <p>{weather.data.main.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+          </div>
+        )
+       }
     </div>
-  );
+  ); 
 }
 
 export default App;
